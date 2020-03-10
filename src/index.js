ae1e4bd74d93218b5fd5b9e9fed8aa97bf4be6d0
Viktoriia Kamorina <48948022+VictoriaKam@users.noreply.github.com>
2020-03-10 16:42:36 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let arr = Array.from(String(n));
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!isNaN(arr[i])) {
+            newArr.unshift(arr[i]);
+        }
+    }
+    return Number(newArr.join(''));
 }
